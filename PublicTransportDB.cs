2e49921293571f2f3802db0d7c5ebea99993c73d
Sergey Gukov <47539180+bruimafia@users.noreply.github.com>
2023-04-10 22:28:46 +0300

    Add PublicTransportDB.cs

---
+++ PublicTransportDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solid
+{
+    class PublicTransportDB
+    {
+        public void SaveTransport(PublicTransport transport)
+        {
+            ///
+        }
+
+        public void DeleteTransport(PublicTransport transport)
+        {
+            ///
+        }
+    }
+}
